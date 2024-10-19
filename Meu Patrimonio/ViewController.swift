d63408f4b9b8ee317fd5c936fe697f40e3145f39
Jobson Batista <jobson2084@gmail.com>
2024-10-19 14:11:01 -0300

    Initial Commit

---
+++ Meu Patrimonio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meu Patrimonio
+//
+//  Created by Jobson Batista on 19-10-2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
